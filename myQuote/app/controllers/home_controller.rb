b4f9477e2fedc2583fbfe46c6ba1e6acb1690acc
mshub <monte.shub@gmail.com>
2023-10-15 12:46:35 +0800

    small update

--- myQuote/app/controllers/home_controller.rb
+++ myQuote/app/controllers/home_controller.rb
@@ -2,4 +2,7 @@ class HomeController < ApplicationController
   def index
     @quotes = Quote.includes(:source).take(5)
   end
+  def uqoutes
+    @quotes = Quote.include(:sources, :categories).where(user_id: session[:user_id])
+  end
 end
