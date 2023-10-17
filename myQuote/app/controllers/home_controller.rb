ff1a04fecb91b89ab8395d29b11426e1db3660f9
mshub <monte.shub@gmail.com>
2023-10-17 12:55:47 +0800

    nearly there

--- myQuote/app/controllers/home_controller.rb
+++ myQuote/app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @quotes = Quote.includes(:source).take(5)
+    @quotes = Quote.includes(:source).reverse_order.take(5)
   end
   def uquotes
     @quotes = Quote.includes(:source, :categories).where(user_id: session[:user_id])
